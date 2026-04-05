a32405945f4229c886a91af48ed0cef179f09309
NeuralEquation <jamjam114514@outlook.jp>
2026-04-05 16:39:32 +0900

    Add files via upload

---
+++ data/restaurant-menus.js
@@ -0,0 +1,725 @@
+window.TDS_RESTAURANT_MENUS = [
+  {
+    "id": "rest-400-mh-roast-chicken",
+    "restaurantId": "rest-400-mh",
+    "name": "ローストチキン",
+    "price": 1850,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-400-mh-seafood-penne-gratin",
+    "restaurantId": "rest-400-mh",
+    "name": "シーフードのペンネグラタン",
+    "price": 1680,
+    "genres": ["パスタ", "シーフード"]
+  },
+  {
+    "id": "rest-400-mh-gnocchi-meat-sauce-hamburg",
+    "restaurantId": "rest-400-mh",
+    "name": "ニョッキ、ミートソースとハンバーグ添え",
+    "price": 1680,
+    "genres": ["パスタ", "肉"]
+  },
+  {
+    "id": "rest-400-mh-raspberry-lemon-mousse-cake",
+    "restaurantId": "rest-400-mh",
+    "name": "ラズベリー＆レモンムースケーキ",
+    "price": 650,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-400-mh-strawberry-milk-mousse-cake",
+    "restaurantId": "rest-400-mh",
+    "name": "ストロベリーミルクムースケーキ",
+    "price": 550,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-400-mh-cheese-mousse-cake",
+    "restaurantId": "rest-400-mh",
+    "name": "チーズムースケーキ",
+    "price": 550,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-403-mh-meatball-spaghetti",
+    "restaurantId": "rest-403-mh",
+    "name": "ミートボール・スパゲッティ",
+    "price": 1200,
+    "genres": ["パスタ", "肉"]
+  },
+  {
+    "id": "rest-403-mh-vegetable-spaghetti",
+    "restaurantId": "rest-403-mh",
+    "name": "スパゲッティ、ベジタブルソース",
+    "price": 1200,
+    "genres": ["パスタ"]
+  },
+  {
+    "id": "rest-403-mh-seafood-cream-spaghetti",
+    "restaurantId": "rest-403-mh",
+    "name": "シーフードクリームのスパゲッティ",
+    "price": 1300,
+    "genres": ["パスタ", "シーフード"]
+  },
+  {
+    "id": "rest-403-mh-seafood-pizza",
+    "restaurantId": "rest-403-mh",
+    "name": "シーフードピッツァ（エッグ＆サーモントラウト）",
+    "price": 1400,
+    "genres": ["ピザ", "シーフード"]
+  },
+  {
+    "id": "rest-403-mh-quattro-formaggi-pizza",
+    "restaurantId": "rest-403-mh",
+    "name": "クワトロ・フォルマッジピッツァ",
+    "price": 1400,
+    "genres": ["ピザ"]
+  },
+  {
+    "id": "rest-403-mh-little-green-man",
+    "restaurantId": "rest-403-mh",
+    "name": "リトルグリーンまん",
+    "price": 400,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-401-mh-choco-donut",
+    "restaurantId": "rest-401-mh",
+    "name": "チョコドーナツ",
+    "price": 450,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-412-mh-chef-course",
+    "restaurantId": "rest-412-mh",
+    "name": "シェフのおすすめコース",
+    "price": 12000,
+    "genres": ["肉", "シーフード"]
+  },
+  {
+    "id": "rest-412-mh-magellan-course",
+    "restaurantId": "rest-412-mh",
+    "name": "マゼラン",
+    "price": 8000,
+    "genres": ["肉", "シーフード"]
+  },
+  {
+    "id": "rest-408-mh-squid-ink-tagliolini",
+    "restaurantId": "rest-408-mh",
+    "name": "イカ墨のタリオリーニ、海老と蟹のトマトクリームソース",
+    "price": 2200,
+    "genres": ["パスタ", "シーフード"]
+  },
+  {
+    "id": "rest-408-mh-beef-ragu-spaghetti",
+    "restaurantId": "rest-408-mh",
+    "name": "スパゲッティ、牛肉のラグーソース",
+    "price": 2200,
+    "genres": ["パスタ", "肉"]
+  },
+  {
+    "id": "rest-408-mh-margherita-pizza",
+    "restaurantId": "rest-408-mh",
+    "name": "ピッツァ･マルゲリータ",
+    "price": 2800,
+    "genres": ["ピザ"]
+  },
+  {
+    "id": "rest-408-mh-palazzo-canal-pizza",
+    "restaurantId": "rest-408-mh",
+    "name": "ピッツァ、パラッツォ・カナル",
+    "price": 3000,
+    "genres": ["ピザ", "肉"]
+  },
+  {
+    "id": "rest-408-mh-tiramisu",
+    "restaurantId": "rest-408-mh",
+    "name": "ティラミス",
+    "price": 800,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-411-mh-mickey-macaron-chocolate",
+    "restaurantId": "rest-411-mh",
+    "name": "ミッキーマカロン（チョコレート）",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-416-mh-smoked-turkey-leg",
+    "restaurantId": "rest-416-mh",
+    "name": "スモークターキーレッグ",
+    "price": 1200,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-423-aw-hot-dog",
+    "restaurantId": "rest-423-aw",
+    "name": "ホットドッグ",
+    "price": 500,
+    "genres": ["パン", "肉"]
+  },
+  {
+    "id": "rest-436-aw-black-pepper-teriyaki-chicken-roll",
+    "restaurantId": "rest-436-aw",
+    "name": "ブラックペッパーてりやきチキンロール",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-446-aw-pulled-pork-cheeseburger",
+    "restaurantId": "rest-446-aw",
+    "name": "プルドポーク･チーズバーガー",
+    "price": 850,
+    "genres": ["バーガー", "肉"]
+  },
+  {
+    "id": "rest-446-aw-bacon-egg-burger",
+    "restaurantId": "rest-446-aw",
+    "name": "ベーコンエッグバーガー",
+    "price": 850,
+    "genres": ["バーガー", "肉"]
+  },
+  {
+    "id": "rest-447-aw-wonderful-friendship-menu",
+    "restaurantId": "rest-447-aw",
+    "name": "「ダッフィー＆フレンズのワンダフル・フレンドシップ」メニュー",
+    "price": null,
+    "genres": ["肉", "シーフード", "スイーツ"]
+  },
+  {
+    "id": "rest-441-aw-cheeseburger-sausage",
+    "restaurantId": "rest-441-aw",
+    "name": "チーズバーガーと骨付きソーセージ",
+    "price": 1480,
+    "genres": ["バーガー", "肉"]
+  },
+  {
+    "id": "rest-441-aw-fried-chicken-rice-bowl",
+    "restaurantId": "rest-441-aw",
+    "name": "フライドチキンのライスボウル（BBQソース）",
+    "price": 1180,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-441-aw-combo-plate",
+    "restaurantId": "rest-441-aw",
+    "name": "コンボプレート",
+    "price": 2140,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-441-aw-duffy-cookie-sand",
+    "restaurantId": "rest-441-aw",
+    "name": "ダッフィークッキーサンド（キャラメルクリーム）",
+    "price": 470,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-441-aw-creme-brulee-churro",
+    "restaurantId": "rest-441-aw",
+    "name": "クレームブリュレ風チュロス",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-422-aw-reuben-hot-sand",
+    "restaurantId": "rest-422-aw",
+    "name": "ルーベン･ホットサンド",
+    "price": 1180,
+    "genres": ["パン", "肉"]
+  },
+  {
+    "id": "rest-422-aw-danish-sand",
+    "restaurantId": "rest-422-aw",
+    "name": "デニッシュサンド",
+    "price": 1090,
+    "genres": ["パン", "肉"]
+  },
+  {
+    "id": "rest-422-aw-cheesecake",
+    "restaurantId": "rest-422-aw",
+    "name": "チーズケーキ",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-422-aw-strawberry-milk-mousse-cake",
+    "restaurantId": "rest-422-aw",
+    "name": "ストロベリーミルクムースケーキ",
+    "price": 550,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-422-aw-cheese-mousse-cake",
+    "restaurantId": "rest-422-aw",
+    "name": "チーズムースケーキ",
+    "price": 550,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-430-aw-bone-in-sausage",
+    "restaurantId": "rest-430-aw",
+    "name": "骨付きソーセージ",
+    "price": 400,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-448-aw-duffy-cookie-sand",
+    "restaurantId": "rest-448-aw",
+    "name": "ダッフィークッキーサンド（キャラメルクリーム）",
+    "price": 470,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-449-aw-chocolate-churros",
+    "restaurantId": "rest-449-aw",
+    "name": "チョコレートチュロス",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-498-aw-choco-donut",
+    "restaurantId": "rest-498-aw",
+    "name": "チョコドーナツ",
+    "price": 450,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-440-aw-fresh-fruit-cup",
+    "restaurantId": "rest-440-aw",
+    "name": "フレッシュフルーツカップ",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-435-aw-gyoza-dog",
+    "restaurantId": "rest-435-aw",
+    "name": "ギョウザドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-435-aw-creme-brulee-churro",
+    "restaurantId": "rest-435-aw",
+    "name": "クレームブリュレ風チュロス",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-432-aw-tempura-zen",
+    "restaurantId": "rest-432-aw",
+    "name": "天麩羅膳",
+    "price": 3400,
+    "genres": ["和食", "シーフード"]
+  },
+  {
+    "id": "rest-432-aw-fish-hirekatsu-zen",
+    "restaurantId": "rest-432-aw",
+    "name": "魚介とヒレカツ膳",
+    "price": 3400,
+    "genres": ["和食", "シーフード", "肉"]
+  },
+  {
+    "id": "rest-432-aw-miso-clam-chowder",
+    "restaurantId": "rest-432-aw",
+    "name": "東京ディズニーリゾート40周年“ドリームゴーラウンド”味噌クラムチャウダー",
+    "price": 700,
+    "genres": ["和食", "シーフード"]
+  },
+  {
+    "id": "rest-432-aw-fruits-warabimochi-parfait",
+    "restaurantId": "rest-432-aw",
+    "name": "フルーツわらび餅パフェ",
+    "price": 1500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-450-pd-gyoza-dog",
+    "restaurantId": "rest-450-pd",
+    "name": "ギョウザドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-453-pd-creme-brulee-churro",
+    "restaurantId": "rest-453-pd",
+    "name": "クレームブリュレ風チュロス",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-454-pd-gyoza-dog",
+    "restaurantId": "rest-454-pd",
+    "name": "ギョウザドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-451-pd-mix-fry",
+    "restaurantId": "rest-451-pd",
+    "name": "ミックスフライ",
+    "price": 1380,
+    "genres": ["シーフード", "肉"]
+  },
+  {
+    "id": "rest-451-pd-spicy-chicken-oven",
+    "restaurantId": "rest-451-pd",
+    "name": "スパイシーチキンのオーブン焼き",
+    "price": 1250,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-451-pd-grilled-beef-japanese-sauce",
+    "restaurantId": "rest-451-pd",
+    "name": "グリルドビーフ、和風おろしソース",
+    "price": 2380,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-451-pd-tomato-cream-soup",
+    "restaurantId": "rest-451-pd",
+    "name": "トマトクリームスープ",
+    "price": 500,
+    "genres": ["スープ"]
+  },
+  {
+    "id": "rest-451-pd-chocolate-cake",
+    "restaurantId": "rest-451-pd",
+    "name": "チョコレートケーキ",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-460-lrd-yucatan-sausage-dog",
+    "restaurantId": "rest-460-lrd",
+    "name": "ユカタンソーセージドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-457-lrd-tipotorta-sweet-potato",
+    "restaurantId": "rest-457-lrd",
+    "name": "ティポトルタ（スウィートポテト）",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-457-lrd-tipotorta-chocolate-orange",
+    "restaurantId": "rest-457-lrd",
+    "name": "ティポトルタ(チョコレート＆オレンジ)",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-456-lrd-spicy-smoked-chicken-leg",
+    "restaurantId": "rest-456-lrd",
+    "name": "スパイシースモークチキンレッグ",
+    "price": 800,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-456-lrd-pork-rice-roll",
+    "restaurantId": "rest-456-lrd",
+    "name": "ポークライスロール",
+    "price": 600,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-456-lrd-gyoza-dog",
+    "restaurantId": "rest-456-lrd",
+    "name": "ギョウザドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-459-lrd-hashed-beef-yucatan-rice-bowl",
+    "restaurantId": "rest-459-lrd",
+    "name": "ハッシュドビーフのユカタンライスボウル（骨付きチキン、ベーコン、エッグ）",
+    "price": 1300,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-459-lrd-vegetable-keema-yucatan-rice-bowl",
+    "restaurantId": "rest-459-lrd",
+    "name": "ベジタブルキーマカレーのユカタンライスボウル",
+    "price": 1350,
+    "genres": ["ご飯", "カレー"]
+  },
+  {
+    "id": "rest-452-lrd-chip-dale-man",
+    "restaurantId": "rest-452-lrd",
+    "name": "チップとデールまん（トマトチキン＆さつまいも）",
+    "price": 750,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-452-lrd-little-green-man",
+    "restaurantId": "rest-452-lrd",
+    "name": "リトルグリーンまん",
+    "price": 550,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-461-lrd-spicy-smoked-chicken-leg",
+    "restaurantId": "rest-461-lrd",
+    "name": "スパイシースモークチキンレッグ",
+    "price": 800,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-409-fs-smorrebrod-roast-beef-salmon",
+    "restaurantId": "rest-409-fs",
+    "name": "スモーブロー（ローストビーフ、スモークサーモントラウト、レムラードソース）",
+    "price": 1680,
+    "genres": ["パン", "肉", "シーフード"]
+  },
+  {
+    "id": "rest-409-fs-meatball-rice-brown-sauce",
+    "restaurantId": "rest-409-fs",
+    "name": "ブラウンソースのミートボールライス（ニンジン、ポテト、ズッキーニ）",
+    "price": 1680,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-409-fs-berry-whip-pancake",
+    "restaurantId": "rest-409-fs",
+    "name": "ベリーホイップパンケーキ、メイプルソース付き",
+    "price": 1580,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-410-fs-tiramisu-cookie-sand-ice",
+    "restaurantId": "rest-410-fs",
+    "name": "ティラミス・クッキーサンドアイス",
+    "price": 400,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-413-fs-duckling-dream-burger",
+    "restaurantId": "rest-413-fs",
+    "name": "ダックリングドリームバーガー",
+    "price": 900,
+    "genres": ["バーガー", "肉"]
+  },
+  {
+    "id": "rest-413-fs-duckling-dream-cheese-burger",
+    "restaurantId": "rest-413-fs",
+    "name": "ダックリングドリームチーズバーガー",
+    "price": 950,
+    "genres": ["バーガー", "肉"]
+  },
+  {
+    "id": "rest-413-fs-fried-shrimp-burger",
+    "restaurantId": "rest-413-fs",
+    "name": "フライドシュリンプバーガー",
+    "price": 950,
+    "genres": ["バーガー", "シーフード"]
+  },
+  {
+    "id": "rest-413-fs-plant-based-burger",
+    "restaurantId": "rest-413-fs",
+    "name": "プラントベースバーガー",
+    "price": 950,
+    "genres": ["バーガー"]
+  },
+  {
+    "id": "rest-414-fs-lost-kids-snack-box-butter-curry-chicken",
+    "restaurantId": "rest-414-fs",
+    "name": "ロストキッズスナックボックス（バターカレーチキン）",
+    "price": 900,
+    "genres": ["ご飯", "カレー", "肉"]
+  },
+  {
+    "id": "rest-414-fs-lost-kids-snack-box-lemon-tea-chicken",
+    "restaurantId": "rest-414-fs",
+    "name": "ロストキッズスナックボックス（レモンティーチキン）",
+    "price": 900,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-414-fs-smoke-chicken-leg",
+    "restaurantId": "rest-414-fs",
+    "name": "スモークチキンレッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-414-fs-fried-pizza",
+    "restaurantId": "rest-414-fs",
+    "name": "フライドピザ",
+    "price": 600,
+    "genres": ["ピザ"]
+  },
+  {
+    "id": "rest-414-fs-demiglace-churros",
+    "restaurantId": "rest-414-fs",
+    "name": "デミグラス・チュロス",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-414-fs-lookout-brownie",
+    "restaurantId": "rest-414-fs",
+    "name": "ルックアウトブラウニー",
+    "price": 600,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-463-ac-beef-curry",
+    "restaurantId": "rest-463-ac",
+    "name": "ビーフカリー（中辛）",
+    "price": 1100,
+    "genres": ["カレー", "ご飯", "肉"]
+  },
+  {
+    "id": "rest-463-ac-chicken-curry",
+    "restaurantId": "rest-463-ac",
+    "name": "チキンカリー（甘口）",
+    "price": 900,
+    "genres": ["カレー", "ご飯", "肉"]
+  },
+  {
+    "id": "rest-463-ac-vegetable-curry",
+    "restaurantId": "rest-463-ac",
+    "name": "ベジタブルカリー",
+    "price": 900,
+    "genres": ["カレー", "ご飯"]
+  },
+  {
+    "id": "rest-463-ac-combination-curry",
+    "restaurantId": "rest-463-ac",
+    "name": "コンビネーションカリー",
+    "price": 1540,
+    "genres": ["カレー", "ご飯", "肉", "シーフード"]
+  },
+  {
+    "id": "rest-463-ac-coconut-pudding",
+    "restaurantId": "rest-463-ac",
+    "name": "ココナッツプリン",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-465-ac-churros-chai-sugar",
+    "restaurantId": "rest-465-ac",
+    "name": "チュロス(チャイシュガー)",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-464-ac-beef-cheese-curry-nan",
+    "restaurantId": "rest-464-ac",
+    "name": "ビーフチーズカリーナン",
+    "price": 700,
+    "genres": ["カレー", "肉"]
+  },
+  {
+    "id": "rest-464-ac-sultans-sundae",
+    "restaurantId": "rest-464-ac",
+    "name": "サルタンズサンデー",
+    "price": 850,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-464-ac-long-nan-meat-sauce",
+    "restaurantId": "rest-464-ac",
+    "name": "ロング･ナン（ミートソース）",
+    "price": 500,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-468-ml-seafood-calzone",
+    "restaurantId": "rest-468-ml",
+    "name": "シーフードカルツォーネ",
+    "price": 980,
+    "genres": ["ピザ", "シーフード"]
+  },
+  {
+    "id": "rest-468-ml-sausage-meat-sauce-pizza",
+    "restaurantId": "rest-468-ml",
+    "name": "ソーセージとミートソースのピザ",
+    "price": 950,
+    "genres": ["ピザ", "肉"]
+  },
+  {
+    "id": "rest-468-ml-tortilla-sand",
+    "restaurantId": "rest-468-ml",
+    "name": "チキン＆ベジタブルのトルティーヤサンド",
+    "price": 880,
+    "genres": ["パン", "肉"]
+  },
+  {
+    "id": "rest-418-mi-yodare-chicken-cold-noodles",
+    "restaurantId": "rest-418-mi",
+    "name": "よだれ鶏の冷やし麺",
+    "price": 1540,
+    "genres": ["麺", "肉"]
+  },
+  {
+    "id": "rest-418-mi-shrimp-dumplings",
+    "restaurantId": "rest-418-mi",
+    "name": "海老蒸し餃子",
+    "price": 500,
+    "genres": ["シーフード"]
+  },
+  {
+    "id": "rest-421-mi-gyoza-dog",
+    "restaurantId": "rest-421-mi",
+    "name": "ギョウザドッグ",
+    "price": 600,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-419-mi-demiglace-churros-potato",
+    "restaurantId": "rest-419-mi",
+    "name": "デミグラス・チュロス（ポテト）",
+    "price": 500,
+    "genres": ["スイーツ"]
+  },
+  {
+    "id": "rest-418-mi-mabo-fried-rice",
+    "restaurantId": "rest-418-mi",
+    "name": "マーボー豆腐のあんかけチャーハン",
+    "price": 1280,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-418-mi-charsiu-egg-fried-rice",
+    "restaurantId": "rest-418-mi",
+    "name": "チャーシューと玉子のチャーハン",
+    "price": 980,
+    "genres": ["ご飯", "肉"]
+  },
+  {
+    "id": "rest-418-mi-shrimp-chili",
+    "restaurantId": "rest-418-mi",
+    "name": "海老のチリソース",
+    "price": 980,
+    "genres": ["シーフード"]
+  },
+  {
+    "id": "rest-418-mi-mabo-tofu",
+    "restaurantId": "rest-418-mi",
+    "name": "マーボー豆腐",
+    "price": 980,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-418-mi-fried-chicken-negi-salt",
+    "restaurantId": "rest-418-mi",
+    "name": "鶏の唐揚げ、ネギ塩だれ",
+    "price": 780,
+    "genres": ["肉"]
+  },
+  {
+    "id": "rest-418-mi-raspberry-lemon-mousse-cake",
+    "restaurantId": "rest-418-mi",
+    "name": "ラズベリー＆レモンムースケーキ",
+    "price": 550,
+    "genres": ["スイーツ"]
+  }
+]
+;
+
